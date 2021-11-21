9f2f5be7332dff5d2133d0a7bac21d5a5b3a287d
enyo <enyo@users.noreply.github.com>
2021-11-21 11:21:51 +0000

    deploy: 5577fa08b9c81813496f659f6260708a76a0603f

---
+++ _app/pages/privacy.svelte-c07ea8df.js
@@ -0,0 +1,23 @@
+import{S as X,i as Y,s as Z,e as i,t as d,k as u,c as s,a,g as h,d as r,n as c,b as Q,f as $,D as e,E as V}from"../chunks/vendor-b4262478.js";function ee(U){let n,l,w,k,o,E,_,g,W,A,b,y,P,f,B,M,p,H,G,D,K,N,m,R,S,v,x;return{c(){n=i("div"),l=i("h1"),w=d("Privacy Policy"),k=u(),o=i("h3"),E=d("Datenschutzerkl\xE4rung"),_=u(),g=i("p"),W=d(`Der Schutz der pers\xF6nlichen Daten ist uns ein besonderes Anliegen. Wir verarbeiten deine Daten
+    daher ausschlie\xDFlich auf Grundlage der gesetzlichen Bestimmungen (DSGVO, TKG 2003). In diesen
+    Datenschutzinformationen informieren wir dich \xFCber die wichtigsten Aspekte der Datenverarbeitung
+    im Rahmen unserer Website.`),A=u(),b=i("h3"),y=d("Kontakt mit uns"),P=u(),f=i("p"),B=d(`Wenn du per Formular auf der Website oder per E-Mail Kontakt mit uns aufnimmst, werden deine
+    angegebenen Daten zwecks Bearbeitung der Anfrage und f\xFCr den Fall von Anschlussfragen sechs
+    Monate bei uns gespeichert. Diese Daten geben wir nicht ohne deine Einwilligung weiter!`),M=u(),p=i("h3"),H=d("Newsletter"),G=u(),D=i("p"),K=d(`Du hast die M\xF6glichkeit, \xFCber unsere Website unseren Newsletter zu abonnieren. Hierf\xFCr ben\xF6tigen
+    wir deine E-Mail-Adresse und deine Erkl\xE4rung, dass du mit dem Bezug des Newsletters
+    einverstanden bist.`),N=u(),m=i("h3"),R=d("Deine Rechte"),S=u(),v=i("p"),x=d(`Dir stehen bez\xFCglich Deiner bei uns gespeicherten Daten grunds\xE4tzlich die Rechte auf Auskunft,
+    Berichtigung, L\xF6schung, Einschr\xE4nkung, Daten\xFCbertragbarkeit, Widerruf und Widerspruch zu. Wenn
+    du glaubst, dass die Verarbeitung deiner Daten gegen das Datenschutzrecht verst\xF6\xDFt oder deine
+    datenschutzrechtlichen Anspr\xFCche sonst in einer Weise verletzt worden sind, kannst du dich bei
+    der uns hello@yep-austria.org oder der Datenschutzbeh\xF6rde beschweren.`),this.h()},l(z){n=s(z,"DIV",{class:!0});var t=a(n);l=s(t,"H1",{class:!0});var F=a(l);w=h(F,"Privacy Policy"),F.forEach(r),k=c(t),o=s(t,"H3",{});var I=a(o);E=h(I,"Datenschutzerkl\xE4rung"),I.forEach(r),_=c(t),g=s(t,"P",{});var L=a(g);W=h(L,`Der Schutz der pers\xF6nlichen Daten ist uns ein besonderes Anliegen. Wir verarbeiten deine Daten
+    daher ausschlie\xDFlich auf Grundlage der gesetzlichen Bestimmungen (DSGVO, TKG 2003). In diesen
+    Datenschutzinformationen informieren wir dich \xFCber die wichtigsten Aspekte der Datenverarbeitung
+    im Rahmen unserer Website.`),L.forEach(r),A=c(t),b=s(t,"H3",{});var O=a(b);y=h(O,"Kontakt mit uns"),O.forEach(r),P=c(t),f=s(t,"P",{});var T=a(f);B=h(T,`Wenn du per Formular auf der Website oder per E-Mail Kontakt mit uns aufnimmst, werden deine
+    angegebenen Daten zwecks Bearbeitung der Anfrage und f\xFCr den Fall von Anschlussfragen sechs
+    Monate bei uns gespeichert. Diese Daten geben wir nicht ohne deine Einwilligung weiter!`),T.forEach(r),M=c(t),p=s(t,"H3",{});var j=a(p);H=h(j,"Newsletter"),j.forEach(r),G=c(t),D=s(t,"P",{});var q=a(D);K=h(q,`Du hast die M\xF6glichkeit, \xFCber unsere Website unseren Newsletter zu abonnieren. Hierf\xFCr ben\xF6tigen
+    wir deine E-Mail-Adresse und deine Erkl\xE4rung, dass du mit dem Bezug des Newsletters
+    einverstanden bist.`),q.forEach(r),N=c(t),m=s(t,"H3",{});var C=a(m);R=h(C,"Deine Rechte"),C.forEach(r),S=c(t),v=s(t,"P",{});var J=a(v);x=h(J,`Dir stehen bez\xFCglich Deiner bei uns gespeicherten Daten grunds\xE4tzlich die Rechte auf Auskunft,
+    Berichtigung, L\xF6schung, Einschr\xE4nkung, Daten\xFCbertragbarkeit, Widerruf und Widerspruch zu. Wenn
+    du glaubst, dass die Verarbeitung deiner Daten gegen das Datenschutzrecht verst\xF6\xDFt oder deine
+    datenschutzrechtlichen Anspr\xFCche sonst in einer Weise verletzt worden sind, kannst du dich bei
+    der uns hello@yep-austria.org oder der Datenschutzbeh\xF6rde beschweren.`),J.forEach(r),t.forEach(r),this.h()},h(){Q(l,"class","h1_black"),Q(n,"class","container")},m(z,t){$(z,n,t),e(n,l),e(l,w),e(n,k),e(n,o),e(o,E),e(n,_),e(n,g),e(g,W),e(n,A),e(n,b),e(b,y),e(n,P),e(n,f),e(f,B),e(n,M),e(n,p),e(p,H),e(n,G),e(n,D),e(D,K),e(n,N),e(n,m),e(m,R),e(n,S),e(n,v),e(v,x)},p:V,i:V,o:V,d(z){z&&r(n)}}}class te extends X{constructor(n){super();Y(this,n,null,ee,Z,{})}}export{te as default};
