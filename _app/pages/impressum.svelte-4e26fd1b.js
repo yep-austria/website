b2d1a5ae31812fd254e45a8e66a69a9c39016972
enyo <enyo@users.noreply.github.com>
2021-11-21 14:55:36 +0000

    deploy: 916e8cef609396faba5a873f7b1ac3907a68dcd1

---
+++ _app/pages/impressum.svelte-4e26fd1b.js
@@ -0,0 +1,19 @@
+import{S as ie,i as se,s as ae,e as r,t as s,k as u,c as i,a as h,g as a,d as l,n as d,b as D,f as le,D as e,E as Q}from"../chunks/vendor-a2a2cc66.js";function he(re){let t,m,P,B,o,G,J,S,W,v,I,M,c,R,T,x,y,Y,A,z,H,F,k,Z,j,f,b,q,C,_,K,g,L,N,O;return{c(){t=r("div"),m=r("h1"),P=s("Impressum"),B=u(),o=r("p"),G=s("YEP - Stimme der Jugend"),J=r("br"),S=s(`
+    Verein f\xFCr Mitbestimmung, gesellschaftliche Partizipation und Empowerment von Jugendlichen.`),W=u(),v=r("h3"),I=s("Vereinszweck"),M=u(),c=r("p"),R=s(`Die Vereinst\xE4tigkeit ist nicht auf Gewinn ausgerichtet und bezweckt ausschlie\xDFlich und
+    unmittelbar die F\xF6rderung von Jugendlichen im Bereich Bildung, Selbsterm\xE4chtigung, Teilhabe und
+    Mitbestimmung in der Gesellschaft. `),T=r("br"),x=s(`
+    Der Verein ist unabh\xE4ngig, \xFCberparteilich und dessen T\xE4tigkeit ist nicht auf Gewinn ausgerichtet.
+    Das Ziel ist Jugendliche zu erm\xE4chtigen an gesellschaftlichen Prozessen teilzuhaben, strukturelle
+    Voraussetzungen hierf\xFCr zu erwirken und somit zu einer h\xF6heren Breite an demokratischer Teilhabe
+    in der Gesellschaft zu f\xFChren. `),y=r("br"),Y=s(`
+    Der Verein f\xFChlt sich den Werten der Allgemeinen Erkl\xE4rung der Menschenrechte verpflichtet, agiert
+    werteorientiert auf diesen Grunds\xE4tzen & setzt sich f\xFCr die Verbreitung dieser Werte ein.`),A=u(),z=r("h3"),H=s("YEP - Stimme der Jugend"),F=u(),k=r("p"),Z=s("Wien, 1030"),j=u(),f=r("div"),b=r("a"),q=s("hello@yep-austria.org"),C=u(),_=r("p"),K=u(),g=r("p"),L=s("Verantwortlich f\xFCr Website und Inhalt: "),N=r("br"),O=s("Rebekka Dober"),this.h()},l(E){t=i(E,"DIV",{class:!0});var n=h(t);m=i(n,"H1",{class:!0});var U=h(m);P=a(U,"Impressum"),U.forEach(l),B=d(n),o=i(n,"P",{});var V=h(o);G=a(V,"YEP - Stimme der Jugend"),J=i(V,"BR",{}),S=a(V,`
+    Verein f\xFCr Mitbestimmung, gesellschaftliche Partizipation und Empowerment von Jugendlichen.`),V.forEach(l),W=d(n),v=i(n,"H3",{});var X=h(v);I=a(X,"Vereinszweck"),X.forEach(l),M=d(n),c=i(n,"P",{});var p=h(c);R=a(p,`Die Vereinst\xE4tigkeit ist nicht auf Gewinn ausgerichtet und bezweckt ausschlie\xDFlich und
+    unmittelbar die F\xF6rderung von Jugendlichen im Bereich Bildung, Selbsterm\xE4chtigung, Teilhabe und
+    Mitbestimmung in der Gesellschaft. `),T=i(p,"BR",{}),x=a(p,`
+    Der Verein ist unabh\xE4ngig, \xFCberparteilich und dessen T\xE4tigkeit ist nicht auf Gewinn ausgerichtet.
+    Das Ziel ist Jugendliche zu erm\xE4chtigen an gesellschaftlichen Prozessen teilzuhaben, strukturelle
+    Voraussetzungen hierf\xFCr zu erwirken und somit zu einer h\xF6heren Breite an demokratischer Teilhabe
+    in der Gesellschaft zu f\xFChren. `),y=i(p,"BR",{}),Y=a(p,`
+    Der Verein f\xFChlt sich den Werten der Allgemeinen Erkl\xE4rung der Menschenrechte verpflichtet, agiert
+    werteorientiert auf diesen Grunds\xE4tzen & setzt sich f\xFCr die Verbreitung dieser Werte ein.`),p.forEach(l),A=d(n),z=i(n,"H3",{});var $=h(z);H=a($,"YEP - Stimme der Jugend"),$.forEach(l),F=d(n),k=i(n,"P",{});var ee=h(k);Z=a(ee,"Wien, 1030"),ee.forEach(l),j=d(n),f=i(n,"DIV",{class:!0});var te=h(f);b=i(te,"A",{href:!0});var ne=h(b);q=a(ne,"hello@yep-austria.org"),ne.forEach(l),te.forEach(l),C=d(n),_=i(n,"P",{}),h(_).forEach(l),K=d(n),g=i(n,"P",{});var w=h(g);L=a(w,"Verantwortlich f\xFCr Website und Inhalt: "),N=i(w,"BR",{}),O=a(w,"Rebekka Dober"),w.forEach(l),n.forEach(l),this.h()},h(){D(m,"class","h1_black"),D(b,"href","mailto:hello@yep-austria.org"),D(f,"class","email"),D(t,"class","container")},m(E,n){le(E,t,n),e(t,m),e(m,P),e(t,B),e(t,o),e(o,G),e(o,J),e(o,S),e(t,W),e(t,v),e(v,I),e(t,M),e(t,c),e(c,R),e(c,T),e(c,x),e(c,y),e(c,Y),e(t,A),e(t,z),e(z,H),e(t,F),e(t,k),e(k,Z),e(t,j),e(t,f),e(f,b),e(b,q),e(t,C),e(t,_),e(t,K),e(t,g),e(g,L),e(g,N),e(g,O)},p:Q,i:Q,o:Q,d(E){E&&l(t)}}}class ue extends ie{constructor(t){super();se(this,t,null,he,ae,{})}}export{ue as default};
